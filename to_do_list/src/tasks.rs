ed58503d3416b89025ead213fed85c84c01c4a6a
Somashekhar <somashekharitagi1406@gmail.com>
2024-09-19 12:06:11 +0530

    Create new Task

---
+++ to_do_list/src/tasks.rs
@@ -0,0 +1,18 @@
+use chrono::{serde::ts_seconds, DateTime, Local, Utc};
+use serde::Deserialize;
+use serde::Serialize;
+
+#[derive(Debug, Deserialize, Serialize)]
+pub struct Task{
+    pub text: String,
+    #[serde(with = "ts_seconds")]
+    pub created_at: DateTime<Utc>,
+}
+
+impl Task {
+ 
+      pub fn new(text: String)->Task {
+        let created_at : DateTime<Utc> = Utc::now();
+        Task{text , created_at}
+      } 
+}
